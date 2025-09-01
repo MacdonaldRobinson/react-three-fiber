bf39cd49a1c852021ffc679617e140bbcf8ed687
Mac <maconly2001@gmail.com>
2025-08-31 18:59:16 -0600

    Updated to fix workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the current mode
   const env = loadEnv(mode, process.cwd(), 'VITE_');
+console.log('mode:', mode);
+console.log('env:', env);
+    console.log('Loaded VITE_BASE_PATH:', env.VITE_BASE_PATH) // debug
+
   return {
     plugins: [react()],
-        base: env.VITE_BASE_PATH || '/', // Set the base path dynamically from the environment variable
-
+    base: env.VITE_BASE_PATH || '/', // Set the base path dynamically from the environment variable
   }
 })
